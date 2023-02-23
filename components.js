971a579a3999b4e05d6ec79f5b99d088ac1bff6e
Hegedus Zoltan <zoltan.hegedus88@gmail.com>
2023-02-23 22:11:28 +0200

    styled movie page icons in ratings component

--- components.js
+++ components.js
@@ -4,8 +4,8 @@ const IMDB_URL = `https://www.imdb.com/title/${IMDB_PLACEHOLDER}/?ref_=fn_al_tt_
 const ICONS_MAP = new Map();
 ICONS_MAP.set("IMDB", "fa-solid fa-star")
 ICONS_MAP.set("Internet Movie Database", "fa-solid fa-database")
-ICONS_MAP.set("Rotten Tomatoes", "fa-solid fa-apple")
-ICONS_MAP.set("Metacritic", "fa-solid fa-pen")
+ICONS_MAP.set("Rotten Tomatoes", "fa-solid fa-apple-whole")
+ICONS_MAP.set("Metacritic", "fa-solid fa-pen-nib")
 
 console.log(ICONS_MAP)
 
