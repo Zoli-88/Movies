59a06513af58d06a225842ee82893c1f65c93933
Zoltán H <zoltan.h@wavemyth.com>
2023-02-28 17:17:34 +0200

    started creating scss folder structure and moving styles to corresponding partial files and folders

--- api.js
+++ api.js
@@ -1,7 +1,19 @@
 const BASE_URL = "https://www.omdbapi.com/?apikey=4c8a6d5d";
 const DEFAULT_SEARCH = "s=iron man";
 
+// const LIST_MOVIES = [
+//   "movie1",
+//   "movie2",
+//   ...
+//   "movien"
+// ]
+
+// function getRandomFromList(list) {
+//   return randomItem;
+// }
+
 async function listMovies() {
+  // const response = await fetch(`${BASE_URL}&s=${getRandomFromList(LIST_MOVIES)}`);
   const response = await fetch(`${BASE_URL}&${DEFAULT_SEARCH}`);
   const data = await response.json();
 
--- swiper.js
+++ swiper.js
@@ -3,7 +3,7 @@ function activateSwiper() {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 4,
+    // slidesPerView: 4,
     centeredSlides: true,
     spaceBetween: 30,
     // Navigation arrows
@@ -11,5 +11,22 @@ function activateSwiper() {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 30
+      }
+    }
   });
 }
