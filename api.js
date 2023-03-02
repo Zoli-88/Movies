e1b70bc664e20d6c3769a651cccd36cec77c32ce
Hegedus Zoltan <zoltan.hegedus88@gmail.com>
2023-03-02 20:27:25 +0200

    testing changes made at home to develop branch

--- api.js
+++ api.js
@@ -20,22 +20,17 @@ function getRandomFromList(list) {
 
 async function listMovies(movieTitle) {
   const response = await fetch(`${BASE_URL}&s=${movieTitle}`);
-  console.log('response2',response);
   const data = await response.json();
 
-  console.log('data3',data);
   if(data.Response === "False") {
     throw data.Error;
   }
-  console.log('data2',data);
   return data
 }
 
 async function listMovie(imdbID) {
   const response = await fetch(`${BASE_URL}&i=${imdbID}&plot=full`);
-  console.log('response',response);
   const data = await response.json();
-  console.log('data',data);
   if(data.Response === "False") {
     throw data.Error;
   }
