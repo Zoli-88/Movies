c071b9094b7cea7141850749dfbdd990edcfad45
Hegedus Zoltan <zoltan.hegedus88@gmail.com>
2023-08-17 22:48:00 +0300

    centered movie cards on small devices and created a new scss file for the search form component

--- menu-nav.js
+++ menu-nav.js
@@ -1,11 +1,15 @@
 function menuNavComponent() {
   const $navMenuComponentLinks = document.querySelector(".navigation-component .nav-links");
-  const breakpoint = window.matchMedia("(max-width: 768px)");
+  const $navMenuToggleButton = document.querySelector(".navigation-component .menu-nav-toggle");
+  const breakpointValue = 768;
+  const breakpoint = window.matchMedia(`(max-width: ${breakpointValue}px)`);
   
   function handleMenuNavLayout(event) {
     if (event.matches) {
       $navMenuComponentLinks.classList.add("hide");
+      $navMenuToggleButton.classList.remove("hide");
     } else {
+      $navMenuToggleButton.classList.add("hide");
       $navMenuComponentLinks.classList.remove("hide");
     }
   }
