c85da2641239fc370bc24ec9dbe7a90b07fc9a97
Hegedus Zoltan <zoltan.hegedus88@gmail.com>
2023-08-22 23:19:02 +0300

    fixed form component to function on home and movie detail pages

--- js/render/home-page/movies.js
+++ js/render/home-page/movies.js
@@ -1,6 +1,6 @@
-import { listMovies } from "../../api/api.js";
-import { cardComponent } from "../../components/_card.js";
-import { renderError } from "../shared/error.js";
+import { listMovies } from "../api/api.js";
+import { cardComponent } from "../components/_card.js";
+import { renderError } from "./shared/error.js";
 
 async function renderMovies(searchResult) {
     const $container = document.querySelector("#container");
