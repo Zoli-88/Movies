071933014bb4aa2fc93a7582c59c82e8f720b361
Hegedus Zoltan <zoltan.hegedus88@gmail.com>
2023-08-22 22:18:33 +0300

    added event listener to the back to previous page component

---
+++ js/components/_back-to-previous-page.js
@@ -0,0 +1,9 @@
+function backToPreviousPageComponent() {
+    return `
+    <div class="back-to-previous-page-component">
+        <button class="primary-btn">Back</button>
+    </div>
+    `
+}
+
+export {backToPreviousPageComponent};
--- js/render/movie-details-page/movie.js
+++ js/render/movie-details-page/movie.js
@@ -2,9 +2,12 @@ import {renderError} from "../shared/error.js";
 import {listMovie} from "../../api/api.js";
 import {ratingsComponent} from "../../components/_ratings.js";
 import {descriptionComponent} from "../../components/_description.js";
+import {backToPreviousPageComponent} from "../../components/_back-to-previous-page.js";
+import { handleBackPage } from "../../utils/utils.js";
 
 async function renderMovie() {
     const $moviePage = document.querySelector("#movie-page");
+    const $backToPreviousPage = document.querySelector("#prev-page");
     const $description = document.querySelector("#description");
 
     const paramsString = window.location.search;
@@ -14,6 +17,8 @@ async function renderMovie() {
 
     try {
         const movie = await listMovie(queryImdbID);
+        $backToPreviousPage.innerHTML = backToPreviousPageComponent();
+        $backToPreviousPage.addEventListener("click", handleBackPage);
         $moviePage.innerHTML = ratingsComponent(movie, querySearchPhrase);
         $description.innerHTML = descriptionComponent(movie, querySearchPhrase);
     } catch (error) {
