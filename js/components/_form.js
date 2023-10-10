811299562dfae9018c60de447364082906ee1569
Zoltan H <zoltan.h@wavemyth.com>
2023-10-10 17:54:01 +0300

    change form input id based on the screen size

--- js/components/_form.js
+++ js/components/_form.js
@@ -1,5 +1,16 @@
-import {searchFormValidation} from "../utils/utils.js";
-const $formComponent = document.querySelector("#search-form");
+import {searchFormValidation, checkIfMobileOrDesktop} from "../utils/utils.js";
+let $formComponent;
+const breakpointValue = 768;
+
+function updateFormIdBasedOnScreenSize(isDesktop) {
+  if (isDesktop) {
+    $formComponent = document.querySelector(".navigation-component .desktop #search-form");
+    $formComponent.addEventListener("submit", searchForm);
+  } else {
+    $formComponent = document.querySelector(".navigation-component .mobile #search-form");
+    $formComponent.addEventListener("submit", searchForm);
+  }
+}
 
 function searchForm(event) {
   event.preventDefault();
@@ -8,4 +19,4 @@ function searchForm(event) {
   searchFormValidation(searchResult);
 }
 
-$formComponent.addEventListener("submit", searchForm);
+checkIfMobileOrDesktop(updateFormIdBasedOnScreenSize, breakpointValue);
