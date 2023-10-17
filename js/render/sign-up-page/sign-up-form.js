888e88eb061e1d7302dc019aa0d014b4b6099f46
Zoltan H <zoltan.h@wavemyth.com>
2023-10-17 17:20:23 +0300

    started implementing sign up form

--- js/render/sign-up-page/sign-up-form.js
+++ js/render/sign-up-page/sign-up-form.js
@@ -3,9 +3,19 @@ import {handleBackPage} from "../../utils/utils.js";
 
 function renderSignUpForm() {
   const $backToPreviousPageButton = document.querySelector("#prev-page-btn");
-  $backToPreviousPageButton.addEventListener("click", handleBackPage);
   const $formContainer = document.querySelector("#sign-up-form-container");
   $formContainer.insertAdjacentHTML("afterbegin", signUpFormComponent());
+  
+  const $form = document.querySelector("#sign-up-form");
+
+  $backToPreviousPageButton.addEventListener("click", handleBackPage);
+  $form.addEventListener("submit", handleSubmit);
+}
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const userData = new FormData(event.target);
+  console.log(userData);
 }
 
 export {renderSignUpForm}
