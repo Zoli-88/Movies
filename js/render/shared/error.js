0240c4a9b8ef6c4d15a0ac4d7fe51edbe82f4e36
Zoltan H <zoltan.h@wavemyth.com>
2023-11-03 15:30:03 +0200

    added some error handling in the renderError function

--- js/render/shared/error.js
+++ js/render/shared/error.js
@@ -3,23 +3,33 @@ import { errorComponent } from "../../components/_error.js";
 function renderError(error) {
     const $main = document.querySelector("main");
     let personalisedErrorMessage = "";
-    if (error === "Movie not found!") {
-        personalisedErrorMessage = "Please try searching for a different title";
-        $main.insertAdjacentHTML("beforeend", errorComponent(error, personalisedErrorMessage));
-        const $errorButton = document.querySelector("#error-btn");
-            if ($errorButton) $errorButton.addEventListener("click", () => {
-                // redirectUserToTheHomePage();
-                clearError();
-            });
-    } else {
-        redirectUserToTheErrorPage(error);
+    console.log(error)
+
+    switch (error) {
+        case "Movie not found!":
+            personalisedErrorMessage = `Please try searching for a different title`;
+            break;
+        case "Too many results.":
+            personalisedErrorMessage = `Please try searching for a more specific title to narrow your search`;
+            break;
+        case "Incorrect IMDb ID.":
+            personalisedErrorMessage = `Please try searching for a title instead, for example "Thor"`;
+            break;
+        default:
+            redirectUserToTheErrorPage(error);
+            return;
     }
+    
+    $main.insertAdjacentHTML("beforeend", errorComponent(error, personalisedErrorMessage));
+    const $errorButton = document.querySelector("#error-btn");
+    if ($errorButton) $errorButton.addEventListener("click", () => {
+        clearError();
+    });
 }
   
 function redirectUserToTheErrorPage(error) {
     const redirectUrl = `/error.html?errorMessage=${encodeURIComponent(error)}`;
     window.location.href = redirectUrl;
-    clearError();
 }
   
 function clearError() {
